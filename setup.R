e5daa5679cbdb414749395c42afbef1c5e0dde9d
Victor <victor.2wy@gmail.com>
2022-11-07 10:51:09 -0800

    add some biconductor packages to setup.R

--- setup.R
+++ setup.R
@@ -1,2 +1,6 @@
 install.packages("tidyverse")
 install.packages("Seurat")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install(c("genefilter","ballgown","edgeR","GenomicRanges","rhdf5","biomaRt","scran","sva","gage","org.Hs.eg.db"))
