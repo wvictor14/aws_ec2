f4740f01af99355524584c3b199af917c6495721
Victor Yuan <vyuan@sonomabio.com>
2023-11-02 12:13:36 -0700

    add omz install to setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt-get upgrade
 sudo timedatectl set-timezone America/Los_Angeles
 
 ##############################
-## attach scratch space 
+## attach scratch space     ##
 ##############################
 # Create mountpoint for additional storage volume
 cd /
@@ -32,6 +32,28 @@ sudo chown -R ubuntu:ubuntu /workspace
 # Create symlink to the added volume in your home directory
 cd ~
 ln -s /workspace workspace
+
+##############################
+##          ohmyzsh         ##
+##############################
+
+# install
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# verify version
+zsh --version #5.8.1
+
+# make default shell if not applied via script
+chsh -s $(which zsh)
+# test it is default
+echo $SHELL
+
+# copy .zshrc custom settings
+code ~/.zshrc
+
+# syntax highlightting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 ##############################
 ##           git            ##
